b56048e00dc25742637f458004e57443504efa23
nunik2255 <45875868+nunik2255@users.noreply.github.com>
2018-12-30 14:29:17 +0700

    Add files via upload

---
+++ MENGHITUNG LUAS PERSEGI PANJANG.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p,l;
+    cout<<"MENGHITUNG LUAS PERSEGI PANJANG"<<endl;
+    cout<<"masukan panjang : " ;
+    cin>>p;
+    cout<<"Masukan lebar : " ;
+    cin>>l;
+    cout<<"Luas Persegi Panjang Adalah = "<<p*l<<"\n";
+    return 0;
+}
